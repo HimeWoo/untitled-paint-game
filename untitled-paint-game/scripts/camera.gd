224b71674e3caf163e1c4d1215cf7639df1ba64d
HimeWoo <aogata15@gmail.com>
2025-12-03 14:32:06 -0800

    larger room transitions

--- untitled-paint-game/scripts/camera.gd
+++ untitled-paint-game/scripts/camera.gd
@@ -1,2 +1,12 @@
 class_name Camera
 extends Camera2D
+
+@export var zoom_speed: float = 1.0
+var target_zoom: Vector2 = zoom
+
+
+func _process(delta: float) -> void:
+  if not (target_zoom - zoom).is_zero_approx():
+    zoom = zoom.lerp(target_zoom, delta * zoom_speed)
+
+
