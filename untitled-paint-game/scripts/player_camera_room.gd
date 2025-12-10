81f65c63090efeba29ab1165d1c37c58e7fa94d4
HimeWoo <aogata15@gmail.com>
2025-12-10 04:27:04 -0800

    added more camera controls

--- untitled-paint-game/scripts/player_camera_room.gd
+++ untitled-paint-game/scripts/player_camera_room.gd
@@ -5,6 +5,12 @@ extends Area2D
 @export var camera: Camera
 ## The target for [member camera] to follow
 @export var target: Node2D
+## New smoothing value for [member camera] when entered
+@export_custom(PROPERTY_HINT_NONE, "suffix:px/s") var cam_smoothing: float = 0
+## New zoom of the camera when entered
+@export_custom(PROPERTY_HINT_LINK, "suffix:") var camera_zoom: Vector2 = Vector2.ZERO
+## New zoom speed of the camera when entered
+@export var zoom_speed: float = 0
 ## The area [member camera] is allowed to move in
 @export var camera_area: CollisionShape2D
 ## If true then [member camera]'s x position will be constant with value [member locked_x_value]
@@ -18,6 +24,9 @@ extends Area2D
 
 @onready var _rect_pos: Vector2 = camera_area.shape.get_rect().position + camera_area.global_position
 @onready var _rect_end: Vector2 = camera_area.shape.get_rect().end + camera_area.global_position
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
 
 
 func _process(_delta: float) -> void:
@@ -43,3 +52,13 @@ func _process(_delta: float) -> void:
 			camera_y = target.position.y
 
 		camera.position = Vector2(camera_x, camera_y)
+
+
+func _on_body_entered(body: Node2D):
+	if body.is_in_group("player"):
+		if not camera_zoom.is_zero_approx():
+			camera.zoom = camera_zoom
+		if not is_zero_approx(zoom_speed):
+			camera.zoom_speed = zoom_speed
+		if not is_zero_approx(cam_smoothing):
+			camera.position_smoothing_speed = cam_smoothing
